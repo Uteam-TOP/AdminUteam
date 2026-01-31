4f58abb03f7c3215a3adeaf3faf05e7239d61df5
unknown <timezero999@gmail.com>
2026-01-31 18:23:40 +0300

    fix: prod env fix

--- src/environment.prod.ts
+++ src/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://uteam.top/api',
-  url: 'https://uteam.top',
+  mainUrl: 'https://uteam.top',
   userNameBot: 'Uteam_top_bot'
 };
